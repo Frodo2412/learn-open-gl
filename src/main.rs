f4bf0cd2bf606ed95ecf7dcef41c5cd88061c05e
Bruno <brunolemus553@gmail.com>
2024-01-11 12:33:30 -0800

    feat: create `sdl` with `gl` context

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use beryllium::{init, Sdl, video};
+
+fn setup_gl_context() -> Sdl {
+    let sdl = Sdl::init(init::InitFlags::EVERYTHING);
+    sdl.set_gl_context_major_version(3).unwrap();
+    sdl.set_gl_context_major_version(3).unwrap();
+    sdl.set_gl_profile(video::GlProfile::Core).unwrap();
+    #[cfg(target_os = "macos")]
+    {
+        sdl
+            .set_gl_context_flags(video::GlContextFlags::FORWARD_COMPATIBLE)
+            .unwrap();
+    }
+    sdl
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = setup_gl_context();
 }
