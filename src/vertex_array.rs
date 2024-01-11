ad99a2e815589d76e5333bdadf18de8854704ed7
Bruno <brunolemus553@gmail.com>
2024-01-11 14:36:35 -0800

    fix: `VertexArray` struct

---
+++ src/vertex_array.rs
@@ -0,0 +1,24 @@
+use ogl33::{glBindVertexArray, glGenVertexArrays, GLuint};
+
+pub struct VertexArray(pub GLuint);
+
+impl VertexArray {
+    pub fn new() -> Option<Self> {
+        let mut vao = 0;
+        unsafe { glGenVertexArrays(1, &mut vao) };
+        if vao != 0 {
+            Some(Self(vao))
+        } else {
+            None
+        }
+    }
+
+    pub fn bind(&self) {
+        unsafe { glBindVertexArray(self.0) }
+    }
+
+    pub fn clear_binding() {
+        unsafe { glBindVertexArray(0) }
+    }
+}
+
