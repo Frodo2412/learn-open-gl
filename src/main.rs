83ccf57f490ad398e313e5a8a1b9aa129f8573fd
Bruno <brunolemus553@gmail.com>
2024-01-11 12:52:31 -0800

    feat: process events

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,52 @@
-use beryllium::{init, Sdl, video};
+use beryllium::{
+    events::Event,
+    init::InitFlags,
+    video::{CreateWinArgs, GlContextFlags, GlProfile},
+    *,
+};
+use beryllium::video::GlWindow;
+
+const WINDOW_TITLE: &str = "Hello Window";
 
 fn setup_gl_context() -> Sdl {
-    let sdl = Sdl::init(init::InitFlags::EVERYTHING);
+    let sdl = Sdl::init(InitFlags::EVERYTHING);
     sdl.set_gl_context_major_version(3).unwrap();
-    sdl.set_gl_context_major_version(3).unwrap();
-    sdl.set_gl_profile(video::GlProfile::Core).unwrap();
-    #[cfg(target_os = "macos")]
-    {
-        sdl
-            .set_gl_context_flags(video::GlContextFlags::FORWARD_COMPATIBLE)
-            .unwrap();
+    sdl.set_gl_context_minor_version(3).unwrap();
+    sdl.set_gl_profile(GlProfile::Core).unwrap();
+    let mut flags = GlContextFlags::default();
+    if cfg!(target_os = "macos") {
+        flags |= GlContextFlags::FORWARD_COMPATIBLE;
     }
+    if cfg!(debug_asserts) {
+        flags |= GlContextFlags::DEBUG;
+    }
+    sdl.set_gl_context_flags(flags).unwrap();
     sdl
 }
 
-fn create_window(sdl: Sdl) {
-    let win_args = video::CreateWinArgs {
-        title: "LearnOpenGL",
+fn create_window(sdl: &Sdl) -> GlWindow {
+    let win_args = CreateWinArgs {
+        title: WINDOW_TITLE,
         width: 800,
         height: 600,
-        allow_high_dpi: true,
-        borderless: false,
-        resizable: false,
+        ..Default::default()
     };
 
-    let _win = sdl
+    sdl
         .create_gl_window(win_args)
-        .expect("couldn't make a window and context");
+        .expect("couldn't make a window and context")
 }
 
 fn main() {
     let sdl = setup_gl_context();
-    create_window(sdl);
+    let _win = create_window(&sdl);
+
+    'main_loop: loop {
+        while let Some((event, _timestamp)) = sdl.poll_events() {
+            match event {
+                Event::Quit => break 'main_loop,
+                _ => ()
+            }
+        }
+    }
 }
